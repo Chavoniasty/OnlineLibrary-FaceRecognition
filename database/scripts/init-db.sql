a8f734a94027af0fb5512b0ff92c86a4fd9f26d0
dam900 <d.jakubowski@xberry.tech>
2024-06-02 14:56:23 +0200

    db initialization

--- database/scripts/init-db.sql
+++ database/scripts/init-db.sql
@@ -12,6 +12,24 @@ CREATE TABLE IF NOT EXISTS faceslib.faces (
     INDEX group_index (age, gender, emotion, race)
 );
 
+CREATE TABLE IF NOT EXISTS faceslib.users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(60) NOT NULL, -- `BCrypt hash` according to bcrypt.node.docs result is 60 characters long.
+    email VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS faceslib.favourites (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    face_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES faceslib.users(id),
+    FOREIGN KEY (face_id) REFERENCES faceslib.faces(id)
+);
+
+INSERT INTO faceslib.users (username, password, email)
+VALUES ('GUEST_USER', 'GUEST_PASSWORD', 'guest@email.com'); 
+
 /*
 INSERT INTO faceslib.faces (age, race, emotion, gender, facedata)
 VALUES (12, 'white', 'happy', 'man', 'data');
